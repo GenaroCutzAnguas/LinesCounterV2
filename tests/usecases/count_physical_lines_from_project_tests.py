d3d14894a13e517985cd56a72b8bd9c7994a0d86
diegoalamilla <diego.alamilla123@gmail.com>
2025-03-19 10:38:34 -0600

    feat: :sparkles: add physical line count per file

--- tests/usecases/count_physical_lines_from_project_tests.py
+++ tests/usecases/count_physical_lines_from_project_tests.py
@@ -11,21 +11,21 @@ class CountPhysicalLinesFromProjectTests(TestCase):
             "tests/assets/empty_dir"
         )
 
-        self.assertEqual(project_physical_lines_count, 0)
+        self.assertEqual(project_physical_lines_count[0], 0)
 
     def test_that_a_project_with_only_comments_should_return_zero(self):
         project_physical_lines_count = count_physical_lines_from_project(
             "tests/assets/empty_python_project"
         )
 
-        self.assertEqual(project_physical_lines_count, 0)
+        self.assertEqual(project_physical_lines_count[0], 0)
 
     def test_that_a_project_with_only_code_should_count_the_physical_lines(self):
         project_physical_lines_count = count_physical_lines_from_project(
             "tests/assets/only_code_python_project"
         )
 
-        self.assertEqual(project_physical_lines_count, 44)
+        self.assertEqual(project_physical_lines_count[0], 44)
 
     def test_that_a_project_with_code_and_comments_should_count_the_physical_lines(
         self,
@@ -34,4 +34,4 @@ class CountPhysicalLinesFromProjectTests(TestCase):
             "tests/assets/documented_python_project"
         )
 
-        self.assertEqual(project_physical_lines_count, 44)
+        self.assertEqual(project_physical_lines_count[0], 44)
